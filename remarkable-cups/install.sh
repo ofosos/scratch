b45b46c5aa5bcfa0165e9bb149872918d7f70dbd
Sampsa Laapotti <sampsa@laapotti.net>
2019-10-23 21:53:31 +0300

    Add installation script to facillitate installation

---
+++ remarkable-cups/install.sh
@@ -0,0 +1,21 @@
+! /usr/bin/env bash
+
+# Set bash to stop execution on failed commands. For sanity and safety
+set -e
+
+echo "Adding Unofficial remarkable printing driver"
+
+# Compile ppd driver & copy to somewhere where CUPS can find it.
+ppdc remarkable.drv; sudo cp ppd/remarkable.ppd /usr/share/cups/model/
+
+# Copy & rename CUPS backend script
+sudo cp remarkable.sh /usr/lib/cups/backend/remarkable
+
+# Secure permissions
+sudo chown root:root /usr/lib/cups/backend/remarkable
+sudo chmod 700 /usr/lib/cups/backend/remarkable
+
+# Add/Update "Remarkable" cups printer
+sudo lpadmin -L 'Cloud Printer' -D 'my remarkable' -p "reMarkable" -E -v 'remarkable:/Print'
+
+echo "All done ׋, happy printing! "
